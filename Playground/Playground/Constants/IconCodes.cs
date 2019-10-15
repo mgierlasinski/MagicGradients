ffac9dd892e8aedc64a3faeed24e9d8f3387a4d1
Marcin Gierlasiński <marcin.gierlasinski@gmail.com>
2019-10-15 09:59:04 +0200

    UI adjusmtments to shell flyout header

--- Playground/Playground/Constants/IconCodes.cs
+++ Playground/Playground/Constants/IconCodes.cs
@@ -3,5 +3,9 @@
     public static class IconCodes
     {
         public const string MagicWand = "\ue900";
+        public const string Play = "\ue901";
+        public const string Gradient = "\ue902";
+        public const string Radial = "\ue903";
+        public const string Gallery = "\ue906";
     }
 }
