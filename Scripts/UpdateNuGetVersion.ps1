9ce342a69e6df43901bdcdb84d12d6ad4954e892
Marcin Gierlasiński <marcin.gierlasinski@gmail.com>
2020-04-27 09:59:46 +0200

    Prepare release 1.1.0

---
+++ Scripts/UpdateNuGetVersion.ps1
@@ -0,0 +1,34 @@
+﻿Function Update-NuGetVersion{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Version,
+        [Parameter(Mandatory=$true)]
+        [String]$File
+        )
+        Replace-LineInFile $Version $File
+}
+
+Function Replace-LineInFile{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [String]$Version,
+        [String]$File
+        )
+         Write-Host "Set new $Version version for $File"
+        $Regex  = '(?<=<Version>)[^<]*'
+        (Get-Content $File) -replace $Regex, $Version | Set-Content $File
+}
+
+Function Update-AllNuGetVersions{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [String]$Version
+        )
+        
+        Update-NuGetVersion $Version '..\MagicGradients\MagicGradients.csproj';
+}
+
+Update-AllNuGetVersions
