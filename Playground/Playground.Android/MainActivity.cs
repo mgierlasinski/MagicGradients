f111ce08eeabd592ab65f25ce955567eac9d2a5c
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2020-05-16 19:38:01 +0200

    #60 Gradient Fixes

--- Playground/Playground.Android/MainActivity.cs
+++ Playground/Playground.Android/MainActivity.cs
@@ -2,6 +2,7 @@
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
+using System;
 
 namespace Playground.Droid
 {
@@ -15,11 +16,32 @@ namespace Playground.Droid
 
             base.OnCreate(savedInstanceState);
 
+#if DEBUG
+            RegisterExceptionHanler();
+#endif
+
             global::Xamarin.Forms.Forms.SetFlags("Shell_Experimental", "Visual_Experimental", "CollectionView_Experimental", "FastRenderers_Experimental");
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+
+#if DEBUG
+        private void RegisterExceptionHanler()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (object sender, UnhandledExceptionEventArgs e) =>
+            {
+                // Record the error in our application logs
+                System.Diagnostics.Debug.WriteLine(
+                    DateTime.Now.ToString() + "  :  " + "[CRITICAL GLOBALLY HANDLED EXCEPTION] - Critical exception has been hit! - Message: " + e.ExceptionObject +
+                        System.Environment.NewLine + System.Environment.NewLine +
+                            "========= Critcal Error has been hit, application closed =========" +
+                                System.Environment.NewLine + System.Environment.NewLine
+                    );
+            };
+        }
+#endif
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
