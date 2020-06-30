8cb07e7a61f34255a691038f93935280ce8eff8e
Marcin Gierlasiński <marcin.gierlasinski@gmail.com>
2020-06-30 17:43:34 +0200

    Change regex

---
+++ Tools/GradientParser/GradientParser.Core/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace GradientParser.Core.Models
+{
+    public class Category
+    {
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("tag")]
+        public string Tag { get; set; }
+
+        [JsonProperty("output")]
+        public string Output { get; set; }
+    }
+}
