0aad3581692caa4ce5c17aee0320d931502be24f
Marcin Gierlasiński <marcin.gierlasinski@gmail.com>
2020-08-12 07:10:03 +0200

    Fix Xamarin.Forms 4.8 crash: background property already exists (#108)
    * Fix Xamarin.Forms 4.8 crash: background property already exists
    * Bump version

--- MagicGradients/StyleSheets.cs
+++ MagicGradients/StyleSheets.cs
@@ -2,8 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
-using System.Text;
 using Xamarin.Forms.StyleSheets;
+using Registrar = Xamarin.Forms.Internals.Registrar;
 
 namespace MagicGradients
 {
@@ -11,25 +11,36 @@ namespace MagicGradients
     {
         public static void RegisterStyle(string name, Type targetType, string bindablePropertyName)
         {
-            var stylePropertyInfo = typeof(Xamarin.Forms.Internals.Registrar).GetProperty("StyleProperties",
-                BindingFlags.Static | BindingFlags.NonPublic);
-            if (stylePropertyInfo == null)
+            var stylePropertyInfo = typeof(Registrar).GetProperty("StyleProperties", BindingFlags.Static | BindingFlags.NonPublic);
+            var styleProperties = stylePropertyInfo?.GetValue(null);
+
+            if (styleProperties == null)
                 return;
 
-            var styleProperties = stylePropertyInfo.GetValue(null);
-
+            var stylePropertiesType = styleProperties.GetType();
             var styleAttributeType = typeof(StyleSheet).Assembly.GetType("Xamarin.Forms.StyleSheets.StylePropertyAttribute");
             var styleAttributeInstance = Activator.CreateInstance(styleAttributeType, name, targetType, bindablePropertyName);
 
-            var dictionaryAdd = styleProperties.GetType().GetMethod("Add");
-            if (dictionaryAdd == null)
+            var containsKeyMethod = stylePropertiesType.GetMethod("ContainsKey");
+            if (containsKeyMethod == null)
                 return;
 
-            var styleListType = typeof(List<>).MakeGenericType(styleAttributeType);
-            var styleList = (IList)Activator.CreateInstance(styleListType);
+            var containsKey = (bool)containsKeyMethod.Invoke(styleProperties, new object[] { name });
+            if (containsKey)
+            {
+                var itemProperty = stylePropertiesType.GetProperty("Item");
+                var attributes = itemProperty?.GetValue(styleProperties, new object[] { name });
 
-            styleList.Add(styleAttributeInstance);
-            dictionaryAdd.Invoke(styleProperties, new object[] { name, styleList });
+                attributes?.GetType().GetMethod("Add")?.Invoke(attributes, new[] { styleAttributeInstance });
+            }
+            else
+            {
+                var styleListType = typeof(List<>).MakeGenericType(styleAttributeType);
+                var styleList = (IList)Activator.CreateInstance(styleListType);
+                styleList.Add(styleAttributeInstance);
+
+                stylePropertiesType.GetMethod("Add")?.Invoke(styleProperties, new object[] { name, styleList });
+            }
         }
     }
 }
