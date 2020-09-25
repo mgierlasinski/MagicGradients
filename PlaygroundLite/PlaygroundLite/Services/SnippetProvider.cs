5197565fa3c14251562d34980da43b8800b38704
Marcin Gierlasiński <marcin.gierlasinski@gmail.com>
2020-09-25 14:03:31 +0200

    Add more snippets

--- PlaygroundLite/PlaygroundLite/Services/SnippetProvider.cs
+++ PlaygroundLite/PlaygroundLite/Services/SnippetProvider.cs
@@ -8,16 +8,32 @@ namespace PlaygroundLite.Services
         {
             new CssSnippet
             {
-                Name = "Linear gradient",
-                Code = string.Format("linear-gradient(90deg, {0} 0%, {1} 50%, {2} 100%)",
+                Name = "Linear (colors only)",
+                GetCode = () => string.Format("linear-gradient({0}, {1}, {2})",
                     ColorUtils.GetRandom().ToHex(),
                     ColorUtils.GetRandom().ToHex(),
                     ColorUtils.GetRandom().ToHex())
             },
             new CssSnippet
             {
-                Name = "Radial gradient",
-                Code = string.Format("radial-gradient(circle, {0} 0%, {1} 50%, {2} 100%)",
+                Name = "Linear (with angle)",
+                GetCode = () => string.Format("linear-gradient(45deg, {0} 0%, {1} 50%, {2} 100%)",
+                    ColorUtils.GetRandom().ToHex(),
+                    ColorUtils.GetRandom().ToHex(),
+                    ColorUtils.GetRandom().ToHex())
+            },
+            new CssSnippet
+            {
+                Name = "Radial (colors only)",
+                GetCode = () => string.Format("radial-gradient({0}, {1}, {2})",
+                    ColorUtils.GetRandom().ToHex(),
+                    ColorUtils.GetRandom().ToHex(),
+                    ColorUtils.GetRandom().ToHex())
+            },
+            new CssSnippet
+            {
+                Name = "Radial (with shape)",
+                GetCode = () => string.Format("radial-gradient(circle, {0} 0%, {1} 50%, {2} 100%)",
                     ColorUtils.GetRandom().ToHex(),
                     ColorUtils.GetRandom().ToHex(),
                     ColorUtils.GetRandom().ToHex())
