be9dea703be2186849adf8288e186553c323ca44
Marcin Gierlasiński <marcin.gierlasinski@gmail.com>
2020-11-03 19:33:15 +0100

    Fix AssemblyInfo names

--- Playground/Playground.UWP/Properties/AssemblyInfo.cs
+++ Playground/Playground.UWP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UwpShellTest.UWP")]
+[assembly: AssemblyTitle("Playground.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UwpShellTest.UWP")]
+[assembly: AssemblyProduct("Playground.UWP")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
