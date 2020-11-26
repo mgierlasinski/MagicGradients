8665667ad1aa2dc2da0adeb4608029675edbf93d
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2020-11-26 10:07:40 +0100

    Update Icon Resources & Replace with Static Class

--- Playground/Playground/Resources/Fonts/IcoMoon.cs
+++ Playground/Playground/Resources/Fonts/IcoMoon.cs
@@ -15,8 +15,26 @@ namespace Playground.Resources.Fonts
         public const string Code = "\ue907";
         public const string Bolt = "\ue908";
         public const string Paint = "\ue909";
-        public const string Home = "\ue90b";
         public const string Add = "\ue90a";
+        public const string Home = "\ue90b";
+        public const string Clear = "\ue90c";
+        public const string Delete = "\ue90d";
+    }
+    
+    public static class IcoMoonGlyph
+    {
+        public const string MagicWand = "\ue900";
+        public const string Refresh = "\ue901";
+        public const string Gradient = "\ue902";
+        public const string Radial = "\ue903";
+        public const string Palette = "\ue904";
+        public const string Layers = "\ue905";
+        public const string Gallery = "\ue906";
+        public const string Code = "\ue907";
+        public const string Bolt = "\ue908";
+        public const string Paint = "\ue909";
+        public const string Add = "\ue90a";
+        public const string Home = "\ue90b";
         public const string Clear = "\ue90c";
         public const string Delete = "\ue90d";
     }
