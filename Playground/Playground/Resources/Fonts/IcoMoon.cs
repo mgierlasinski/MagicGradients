d7d7099db47b34d253eea966a0441a6bf0fa5410
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2020-11-28 10:16:41 +0100

    Make Icon Codes in the Right Way

--- Playground/Playground/Resources/Fonts/IcoMoon.cs
+++ Playground/Playground/Resources/Fonts/IcoMoon.cs
@@ -5,37 +5,19 @@ namespace Playground.Resources.Fonts
 {
     public partial class IcoMoon
     {
-        public const string MagicWand = "\ue900";
-        public const string Refresh = "\ue901";
-        public const string Gradient = "\ue902";
-        public const string Radial = "\ue903";
-        public const string Palette = "\ue904";
-        public const string Layers = "\ue905";
-        public const string Gallery = "\ue906";
-        public const string Code = "\ue907";
-        public const string Bolt = "\ue908";
-        public const string Paint = "\ue909";
-        public const string Add = "\ue90a";
-        public const string Home = "\ue90b";
-        public const string Clear = "\ue90c";
-        public const string Delete = "\ue90d";
-    }
-    
-    public static class IcoMoonGlyph
-    {
-        public const string MagicWand = "\ue900";
-        public const string Refresh = "\ue901";
-        public const string Gradient = "\ue902";
-        public const string Radial = "\ue903";
-        public const string Palette = "\ue904";
-        public const string Layers = "\ue905";
-        public const string Gallery = "\ue906";
-        public const string Code = "\ue907";
-        public const string Bolt = "\ue908";
-        public const string Paint = "\ue909";
-        public const string Add = "\ue90a";
-        public const string Home = "\ue90b";
-        public const string Clear = "\ue90c";
-        public const string Delete = "\ue90d";
+        public static string MagicWand = "\ue900";
+        public static string Refresh = "\ue901";
+        public static string Gradient = "\ue902";
+        public static string Radial = "\ue903";
+        public static string Palette = "\ue904";
+        public static string Layers = "\ue905";
+        public static string Gallery = "\ue906";
+        public static string Code = "\ue907";
+        public static string Bolt = "\ue908";
+        public static string Paint = "\ue909";
+        public static string Add = "\ue90a";
+        public static string Home = "\ue90b";
+        public static string Clear = "\ue90c";
+        public static string Delete = "\ue90d";
     }
 }
