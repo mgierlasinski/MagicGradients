80dc489260cab74827953a43cb68c4465f60bf39
Marcin Gierlasiński <marcin.gierlasinski@gmail.com>
2021-03-17 07:30:13 +0100

    Add namespace mapping

--- MagicGradients/Properties/AssemblyInfo.cs
+++ MagicGradients/Properties/AssemblyInfo.cs
@@ -1,3 +1,10 @@
 using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 
 [assembly: InternalsVisibleTo("MagicGradients.Tests")]
+
+[assembly: Preserve]
+[assembly: XmlnsDefinition("http://magic-gradients.com/schemas/gradients", "MagicGradients")]
+[assembly: XmlnsDefinition("http://magic-gradients.com/schemas/gradients", "MagicGradients.Animation")]
+[assembly: XmlnsDefinition("http://magic-gradients.com/schemas/gradients", "MagicGradients.Masks")]
