808d1401135ed5e68c01cfb5711179071d4adb92
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2021-04-28 19:06:25 +0200

    Make Playground looks awesome in iPhone 12 mini

--- Playground/Playground/Features/Home/HomePage.xaml.cs
+++ Playground/Playground/Features/Home/HomePage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Xamarin.Forms;
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using Application = Xamarin.Forms.Application;
 
 namespace Playground.Features.Home
 {
@@ -7,6 +11,18 @@ namespace Playground.Features.Home
         public HomePage()
         {
             InitializeComponent();
+            PropertyChanged += ContentPageBase_PropertyChanged;
+        }
+        
+        private void ContentPageBase_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "SafeAreaInsets")
+            {
+                var safeInsets = On<iOS>().SafeAreaInsets();
+                safeInsets.Top = 0;
+                safeInsets.Bottom = safeInsets.Bottom * -1;
+                Application.Current.Resources["SafeAreaInsets"] = safeInsets;
+            }
         }
     }
 }
