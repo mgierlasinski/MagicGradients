9b0a2561b23e0a79e70925a1b4d3f4a2b066604c
Marcin Gierlasiński <marcin.gierlasinski@gmail.com>
2021-05-06 12:15:29 +0200

    Add simple GradientDrawable

---
+++ MagicGradients.Maui/Graphics/GradientDrawable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui.Graphics;
+
+namespace MagicGradients.Maui.Graphics
+{
+    public class GradientDrawable : IDrawable
+    {
+        public void Draw(ICanvas canvas, RectangleF dirtyRect)
+        {
+            var start = new Point(0, 0);
+            var end = new Point(1, 1);
+
+            var paint = new LinearGradientPaint(new[]
+            {
+                new GradientStop(0f, Colors.Orange),
+                new GradientStop(0.5f, Colors.Blue),
+                new GradientStop(1f, Colors.Yellow)
+            }, start, end);
+
+            canvas.SetFillPaint(paint, dirtyRect);
+            canvas.FillRectangle(dirtyRect);
+        }
+    }
+}
