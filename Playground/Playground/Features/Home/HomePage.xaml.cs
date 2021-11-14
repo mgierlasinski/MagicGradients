e6fc151771f3f5c46b3ca9a854f0e603bbbba7c0
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2021-11-14 12:29:57 +0100

    Roll Back Init Safe Area for iOS

--- Playground/Playground/Features/Home/HomePage.xaml.cs
+++ Playground/Playground/Features/Home/HomePage.xaml.cs
@@ -9,6 +9,15 @@ namespace Playground.Features.Home
         public HomePage()
         {
             InitializeComponent();
+            PropertyChanged += ContentPageBase_PropertyChanged;
+        }
+
+        private void ContentPageBase_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "SafeAreaInsets")
+            {
+                this.InitSafeAreaInsets();
+            }
         }
     }
 }
