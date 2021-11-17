3b4d20b59ffbac40ee4e6793ec476ed54c072a0c
Marcin Gierlasiński <marcin.gierlasinski@gmail.com>
2021-11-17 17:52:36 +0100

    Fix AppCenter build scripts (#192)

--- samples/Playground/Playground.iOS/appcenter-post-clone.sh
+++ samples/Playground/Playground.iOS/appcenter-post-clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 SLN_PATH="$APPCENTER_SOURCE_DIRECTORY/MagicGradients.sln"
-UWP_PATH="$APPCENTER_SOURCE_DIRECTORY/Playground/Playground.UWP/Playground.UWP.csproj"
+UWP_PATH="$APPCENTER_SOURCE_DIRECTORY/samples/Playground/Playground.UWP/Playground.UWP.csproj"
 dotnet sln $SLN_PATH remove $UWP_PATH
