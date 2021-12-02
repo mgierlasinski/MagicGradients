61959268b8759d038d356965551621567f3e411a
Marcin Gierlasiński <marcin.gierlasinski@gmail.com>
2021-12-02 11:52:37 +0100

    Add Xaml namespace mapping

---
+++ src/MagicGradients.Forms.Skia/GlobalSetupExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace MagicGradients.Forms.Skia
+{
+    public static class GlobalSetupExtensions
+    {
+        public static GlobalSetup UseSkiaXamlGradients(this GlobalSetup setup)
+        {
+            return setup;
+        }
+    }
+}
---
+++ src/MagicGradients.Forms/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+[assembly: Preserve]
+[assembly: XmlnsDefinition("http://magic.com/schemas/gradients", "MagicGradients")]
+[assembly: XmlnsDefinition("http://magic.com/schemas/gradients", "MagicGradients.Animation")]
+[assembly: XmlnsDefinition("http://magic.com/schemas/gradients", "MagicGradients.Forms")]
+[assembly: XmlnsDefinition("http://magic.com/schemas/gradients", "MagicGradients.Masks")]
