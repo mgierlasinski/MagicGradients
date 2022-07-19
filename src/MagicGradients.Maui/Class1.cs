4c244dd04ed34c10a9c7f7c9429e302ebc56b913
Marcin Gierlasiński <marcin.gierlasinski@gmail.com>
2022-07-19 19:17:09 +0200

    Add Maui Project

---
+++ src/MagicGradients.Maui/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace MagicGradients.Maui
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
---
+++ src/MagicGradients.Maui/Platforms/iOS/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace MagicGradients.Maui
+{
+    // All the code in this file is only included on iOS.
+    public class PlatformClass1
+    {
+    }
+}
